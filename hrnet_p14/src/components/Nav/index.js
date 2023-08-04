295c67241ecd0c613d06cf118613445c10f00d6f
IvayloRachev <rachev.ivaylo@yahoo.fr>
2023-08-04 10:29:13 +0200

    Add Nav component

---
+++ hrnet_p14/src/components/Nav/index.js
@@ -0,0 +1,19 @@
+
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo_wealth_health.jpg";
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <div className="logo-container">
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+      <div className="menu">
+        <NavLink to="/" className="menu-text">Create Employee</NavLink>
+        <NavLink to="/employee-list" className="menu-text">View Employee List</NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
