2bd454f56413ac6cd2b6d24996d1a873e7fcce98
IvayloRachev <rachev.ivaylo@yahoo.fr>
2023-08-04 10:34:37 +0200

    Add Home page

---
+++ hrnet_p14/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+
+import Form from "../../components/FormEmployee";
+const Home = () => {
+  return (
+    <div className="home-page main">
+      <Form />
+    </div>
+  );
+};
+
+export default Home;
